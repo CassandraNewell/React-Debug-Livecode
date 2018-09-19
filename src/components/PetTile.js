74902162cf465bc000572d1227f83f19f33aacaf
Cassandra Newell <cassandra.newell@protonmail.com>
2018-09-19 17:26:43 -0400

    code after livecode

--- src/components/PetTile.js
+++ src/components/PetTile.js
@@ -1,15 +1,23 @@
 import React from 'react'
 
 const PetTile = props => {
+
+  let heartClass
+  if (props.isFavorite) {
+    heartClass = "fas fa-heart fa-5x"
+  } else {
+    heartClass = "far fa-heart fa-5x"
+  }
+
   return(
     <div className="tile row">
       <div className="column small-6">
         <h4>{props.pet.name}</h4>
         <p>{props.pet.species}</p>
         <p>{props.pet.description}</p>
-        <p>
-          <i className="far fa-heart fa-5x"></i>
-        </p>
+        <button>
+          <i className={heartClass} onClick={props.handleClick} ></i>
+        </button>
       </div>
       <div className="column small-6">
         <img width="200px" src={props.pet.image} />
--- src/containers/PetContainer.js
+++ src/containers/PetContainer.js
@@ -1,18 +1,80 @@
-import React from 'react'
+// src/containers/PetContainer.js
+import React, { Component } from 'react'
 import PetTile from '../components/PetTile'
 
-const PetContainer = props => {
-  let pets = props.pets.map(pet => {
-    return(
-      <PetTile key={pet.id} pet={pet} />
-    )
-  })
+class PetContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      favoriteId: null
+    }
+    this.setFavorite = this.setFavorite.bind(this)
+  }
 
-  return(
-    <div className="container">
-      {pets}
-    </div>
-  )
+  setFavorite(newFavoriteId) {
+    // if we didn't care about users trying to favorite already-favorited floofs:
+    // this.setState({favoriteId: newFavoriteId})
+
+    // to handle reclicks (unclicks, whatever you call them)
+    if (this.state.favoriteId === newFavoriteId) {
+      this.setState({favoriteId: null})
+    } else {
+      this.setState({favoriteId: newFavoriteId})
+    }
+  }
+
+  render(){
+    debugger
+    let pets = this.props.pets.map(pet => {
+      let handleClick = () => {
+        this.setFavorite(pet.id)
+      }
+
+      let isFavorite
+      if (pet.id === this.state.favoriteId) {
+        isFavorite = true
+      } else {
+        isFavorite = false
+      }
+
+      // equivalent to the above!
+      // let isFavorite = (pet.id === this.state.favoriteId)
+
+      return(
+        <PetTile
+          key={pet.id}
+          pet={pet}
+          handleClick={handleClick}
+          isFavorite={isFavorite}
+        />
+      )
+    })
+
+    return(
+      <div>
+        {pets}
+      </div>
+    )
+  }
 }
 
 export default PetContainer
+
+// import React from 'react'
+// import PetTile from '../components/PetTile'
+//
+// const PetContainer = props => {
+//   let pets = props.pets.map(pet => {
+//     return(
+//       <PetTile key={pet.id} pet={pet} />
+//     )
+//   })
+//
+//   return(
+//     <div className="container">
+//       {pets}
+//     </div>
+//   )
+// }
+//
+// export default PetContainer
