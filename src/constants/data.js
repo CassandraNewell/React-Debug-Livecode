4661601b11d8058e0ce29a5d25e140007c2a5aec
Cassandra Newell <cassandra.newell@protonmail.com>
2018-12-17 12:54:24 -0500

    add casi's cats

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,57 +1,54 @@
 const animals = {
-  dogs:
-    [
-      {
-        id: 1,
-        breed: "pit/lab",
-        name: "Sir Geoffrey Drake Arrington",
-        image: "https://i.imgur.com/Eyw9YNV.png",
-        human: "AmyLynn"
-      },
-      {
-        id: 2,
-        breed: "ottoman with legs, maybe part hippo",
-        name: "Miss Islay Mae Arrington",
-        image: "https://i.imgur.com/pxG7sIa.png",
-        human: "AmyLynn"
-      }
-    ],
-  cats:
-    [
-      {
-        id: 5,
-        personality: "likes warming his rear on technology",
-        name: "Bingo",
-        image: "http://i.imgur.com/ntz2sQe.jpg",
-        human: "AmyLynn"
-      },
-      {
-        id: 6,
-        personality: "likes mobile tech",
-        name: "Storrow",
-        image: "http://i.imgur.com/oSr7eBY.jpg",
-        human: "AmyLynn"
-      }
-    ],
-  bugs:
-    [
-      {
-        id: 7,
-        species: "Madagascar Hissing Cockroach, (Gromphadorhina portentosa)",
-        personality: "territorial, loves the ladies",
-        name: "Caelian",
-        image: "https://i.imgur.com/QiEIzOw.png",
-        human: "AmyLynn"
-      },
-      {
-        id: 8,
-        species: "Madagascar Hissing Cockroach (Gromphadorhina portentosa)",
-        personality: "mellow, loves to reproduce",
-        name: "Aventine",
-        image: "https://i.imgur.com/37QAsyR.png",
-        human: "AmyLynn"
-      }
-    ]
-  }
+  dogs: [
+    {
+      id: 1,
+      breed: "pit/lab",
+      name: "Sir Geoffrey Drake Arrington",
+      image: "https://i.imgur.com/Eyw9YNV.png",
+      human: "AmyLynn"
+    },
+    {
+      id: 2,
+      breed: "ottoman with legs, maybe part hippo",
+      name: "Miss Islay Mae Arrington",
+      image: "https://i.imgur.com/pxG7sIa.png",
+      human: "AmyLynn"
+    }
+  ],
+  cats: [
+    {
+      id: 3,
+      personality: "has a triangle nose perfect for booping",
+      name: "Juliabeast",
+      image: "https://i.imgur.com/8PjaghN.jpg",
+      human: "Casi"
+    },
+    {
+      id: 4,
+      personality: "too smart for her own good",
+      name: "Catniss",
+      image: "https://i.imgur.com/HY6XJLo.jpg",
+      human: "Casi"
+    }
+  ],
+  bugs: [
+    {
+      id: 5,
+      species: "Madagascar Hissing Cockroach, (Gromphadorhina portentosa)",
+      personality: "territorial, loves the ladies",
+      name: "Caelian",
+      image: "https://i.imgur.com/QiEIzOw.png",
+      human: "AmyLynn"
+    },
+    {
+      id: 6,
+      species: "Madagascar Hissing Cockroach (Gromphadorhina portentosa)",
+      personality: "mellow, loves to reproduce",
+      name: "Aventine",
+      image: "https://i.imgur.com/37QAsyR.png",
+      human: "AmyLynn"
+    }
+  ]
+};
 
 export default animals;
